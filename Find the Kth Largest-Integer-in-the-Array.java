930da2fbf2c0a942f95ab0380d45e6b753030bc3
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-10-15 12:43:15 +0300

    Create Find the Kth Largest-Integer-in-the-Array.java

---
+++ Find the Kth Largest-Integer-in-the-Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+      Arrays.sort(nums,new Comparator<String>(){
+           public int compare(String a , String b){
+               if(a.length() != b.length()) 
+                   return b.length() - a.length();
+               
+               return (b+a).compareTo(a+b);
+           }
+       });
+       return nums[k-1];
+    }
+}
