52c695f5bb5dc70aacbab660e5f54cb0268e7035
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-20 19:51:15 +0300

    Update py-the-captains-room.py

--- py-the-captains-room.py
+++ py-the-captains-room.py
@@ -3,3 +3,12 @@ num = int(input())
 li = list(map(int ,input().split()))
 dic = Counter(li)
 print(min(dic, key = dic.get))
+
+"""num = int(input())
+li = list(map(int ,input().split()))
+dic = dict()
+for x in li:
+    dic[x] = 1 + dic.get(x , 0)
+for key,val in dic.items():
+    if value == 1:
+        print (key)"""
