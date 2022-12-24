55247018149c50fbc221afd8951e0273db1ad544
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-24 11:19:04 +0300

    Time: 33 ms (91.31%), Space: 13.8 MB (98.83%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -2,18 +2,23 @@ class Solution:
     def freqAlphabets(self, s: str) -> str:
         x = len(s)-1
         result = ""
+        
+        
+        # iterate starting from the last chracter
         while x >= 0:
             hold = ""
+            
+           # if there is '#' get he two numbers after it 
             if s[x] == '#':
                 hold = s[x -2]+ s[x-1]
-                print (hold)
                 x -= 3
             elif s[x] != '#':
                 hold = s[x]
                 x -= 1
-                print(hold)
+                
+             # by changinthe strngs in hold to int get the given char by usng 
+              # char method  
             result = chr(96 + int(hold)) + result
-            # result = hold + result
         return result
             
                 
