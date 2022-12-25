e1054fc8e8d808220916301d8cf58f7445bfb99a
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-26 00:29:53 +0300

    Time: 30 ms (93.91%), Space: 13.8 MB (96.33%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,17 +1,22 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
         
-#         find the maximum and minimum values
+#         find the maximum and minimum and sum of values
         min_salary = min(salary)
         max_salary = max(salary)
-        ret = 0.0
-        size = len(salary)
+        sum_salary = sum(salary)
         
-#         and the values other than the max and min to the returned variable
-        for values in salary:
-            if values != min_salary and values != max_salary and size >= 3:
-                ret += values
+#         then substrack theax and min values from sum 
+#         and dividing it to the size - 2to find the avarage 
+        ret = (sum_salary - (min_salary + max_salary)) / (len(salary) -2)
+        # ret = 0.0
+        # size = len(salary)
+        
+# #         and the values other than the max and min to the returned variable
+#         for values in salary:
+#             if values != min_salary and values != max_salary and size >= 3:
+#                 ret += values
                 
 #        divide the value to find the avarage value
-        ret = ret / (size -2)
+        # ret = ret / (size -2)
         return ret
