2f17c8702feab8db912d1686b9fe80f7509685d8
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-26 00:59:56 +0300

    Time: 87 ms (81.12%), Space: 14.7 MB (60.15%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -3,6 +3,7 @@ class Solution:
         ret = []
 #         iterte throgh nums and append f not divisible by two 
 #         and insert  at the left or first index if divible by two
+
         for value in nums:
             if value % 2 == 0:
                 ret.insert(0, value)
