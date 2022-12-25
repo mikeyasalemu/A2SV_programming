aa27c5c9d597057b83b7f239d53268f10ccc7d47
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-26 00:31:06 +0300

    Time: 26 ms (98.04%), Space: 14 MB (9.51%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -9,14 +9,5 @@ class Solution:
 #         then substrack theax and min values from sum 
 #         and dividing it to the size - 2to find the avarage 
         ret = (sum_salary - (min_salary + max_salary)) / (len(salary) -2)
-        # ret = 0.0
-        # size = len(salary)
-        
-# #         and the values other than the max and min to the returned variable
-#         for values in salary:
-#             if values != min_salary and values != max_salary and size >= 3:
-#                 ret += values
-                
-#        divide the value to find the avarage value
-        # ret = ret / (size -2)
+      
         return ret
