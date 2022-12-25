427591d71a8045521d1d1edf89f0548f4dac1770
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-26 00:59:42 +0300

    Time: 90 ms (77.06%), Space: 14.7 MB (60.15%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,11 +1,14 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         ret = []
+#         iterte throgh nums and append f not divisible by two 
+#         and insert  at the left or first index if divible by two
         for value in nums:
             if value % 2 == 0:
                 ret.insert(0, value)
             else:
                 ret.append(value)
+                
         return ret
                 
                 
