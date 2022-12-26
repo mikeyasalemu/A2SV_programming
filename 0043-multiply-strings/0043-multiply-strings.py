8255e273febf37450c8338222de0534fe3c7d3f3
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-26 13:55:00 +0300

    Time: 36 ms (91.40%), Space: 13.8 MB (72.91%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,8 +1,12 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        
+#       initializing a dictionary to covert the string numbers to integer
         value = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
         result1 = 0
         result2 = 0
+        
+#         converting by using the dictionary
         for nums1 in num1:
             result1 = 10* result1 + value[nums1]
         for nums2 in num2:
