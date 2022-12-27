55e1030d18cd21561bb0d7e4b1aaaaeffb9c6c28
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 00:21:21 +0300

    Time: 35 ms (95.15%), Space: 13.8 MB (99.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,18 +7,20 @@ class Solution:
         index2 = 0
         size = len(nums1)
         dif = m
-        # print (dif)
+#         iterate until the size of nums1
         while index < size:
             if index2 == n:
                 break
+                
+#           compare values from both list to put at the exact place
             elif nums2[index2] < nums1[index]:
                 nums1.insert(index, nums2[index2])
-                # index+=1
                 index2+=1
                 dif +=1
                 nums1.pop()
+                
+#           if the numbers remaining in mums2 are all grater than nums1
             elif index >= dif:
-                # nums1.insert(index -1, nums2[index2])
                 nums1[index] = nums2[index2]
                 index2+=1
                 dif +=1
