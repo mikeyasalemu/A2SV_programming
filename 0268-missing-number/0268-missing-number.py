7eb3faad723651843018b2b6ffb364416137daed
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 02:44:45 +0300

    Time: 139 ms (88.98%), Space: 15 MB (98.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-       
-        ret = int( len(nums)*( len(nums) + 1)/2)
+        size = len(nums)
+        
+#       to find the sum of consequative numbers
+        ret = int(size*(size + 1)/2)
         for num in nums:
             ret -= num
         return ret
