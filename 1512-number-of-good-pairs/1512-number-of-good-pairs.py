78746994045ac1fe07f45088da9171dfcb9efbfe
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 02:28:23 +0300

    Time: 34 ms (90.19%), Space: 13.9 MB (56.49%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -3,6 +3,8 @@ class Solution:
         count = 0
         dic = defaultdict(int)
         
+#      assign defaltdict and count the pairs from getting the previous 
+#          dictionary value
         for intiger in nums:
             count += dic[intiger]
             dic[intiger] += 1
