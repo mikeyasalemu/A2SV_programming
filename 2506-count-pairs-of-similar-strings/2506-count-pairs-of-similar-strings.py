886902db5f5508dbafd1b96870e145c60d65dc41
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 02:18:12 +0300

    Time: 89 ms (81.80%), Space: 13.9 MB (72.27%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -3,12 +3,15 @@ class Solution:
         size = len(words)
         hold = []
         
+#       give every word character in words with out repeating and in sorted order 
+#           to another list 
         for index in range(size):
             hold = set(sorted(list(words[index])))
             words[index] = str(hold)
         lst = Counter(words)
-        print(lst)
         ret = 0
+        
+#         using the pairing formula to get the correct number 
         for index in lst:
             
             if lst[index] > 1:
@@ -16,3 +19,12 @@ class Solution:
         
         
         return ret
+#     count = 0
+#         dic = defaultdict(lambda : 0)
+        
+#         for i, word in enumerate(words):
+#             word = "".join(sorted(set(word)))
+#             count += dic[word]
+#             dic[word] += 1
+         
+#         return count
