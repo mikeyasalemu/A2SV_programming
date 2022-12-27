f68d0fc51572e733f5c9ffa7f18b13c705bf4dc7
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 02:26:27 +0300

    Time: 34 ms (90.19%), Space: 13.9 MB (56.49%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,7 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
-        dic = defaultdict(lambda : 0)
+        dic = defaultdict(int)
         
         for intiger in nums:
             count += dic[intiger]
