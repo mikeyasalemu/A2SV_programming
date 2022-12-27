e628535b6da7dcc15c8ef28bece11be93a4f7a21
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-27 14:31:52 +0300

    Time: 55 ms (91.34%), Space: 14.1 MB (15.40%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         hold =[]
+#       give their ouble value to another list
         for index in arr:
             hold.append(index * 2)
             
-        index = 0 
         size = len(arr)
-        temp = 0
+        
+#         check if there is a value that satisfies the given formula 
         for num in range(size):
-           
             if hold[num] in arr and num != arr.index(hold[num]) :
                 return True
             
