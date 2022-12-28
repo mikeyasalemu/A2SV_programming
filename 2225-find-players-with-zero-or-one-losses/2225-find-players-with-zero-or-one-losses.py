4cad546e883140c2fda94bd0fca13a9d8c68a08a
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-28 03:11:45 +0300

    Time: 1872 ms (95.37%), Space: 68.9 MB (42.89%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,11 +1,14 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        
+#       put the players  and the number of matches they lost in the dictionary
         dic = defaultdict(int)
         for winner, loser in matches:
             if winner not in dic:
                     dic[winner] = 0
             dic[loser] +=1
-       
+
+ #     put then in new list according to the given rules   
         win = []
         lost1 = []
         for key, values in dic.items():
