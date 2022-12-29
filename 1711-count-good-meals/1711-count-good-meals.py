6ac0e27723877f8bb7d13ae37e2b65994f3f6b22
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-29 16:29:45 +0300

    Time: 1554 ms (74.79%), Space: 21.7 MB (71.39%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -2,13 +2,21 @@ class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         dic = defaultdict(int)
         count = 0
+        
+#       iterate through the list
         for num1 in deliciousness:
             power_num = 1
+            
+#           maximum it can go is 22 loop
             for _ in range(22): 
                 num2 = power_num - num1
+            
                 if num2 in dic:
+#                     num1 can be paiered only upto the number of time  num2  occurs
                     count += dic[num2]
-                power_num *= 2 
+                power_num *=2
             dic[num1] += 1
+            
+            
         return count % (10**9 + 7)
         
