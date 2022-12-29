e306fc2f9191ce19aba46e55575af0c56c665965
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2022-12-29 19:34:14 +0300

    Time: 774 ms (72.07%), Space: 27.1 MB (36.72%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,21 +1,23 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         dic = Counter(nums)
-        print (dic)
-        # nums.sort()
-        print (nums)
         count = 0
+#         Iterate through the numbers
         for num in nums:
             check = k - num
+            
+#             check holds the difference inorder to check if i present in the number list
+            
             if dic[check] > 0 and dic[num] > 0 and check != num:
+#             decreasing their value as we ave to delete the sums equal to k
+#             for different numbers use different approach
                 count += 1
                 dic[check] -= 1
                 dic[num] -= 1
+            
             elif check == num and dic[check] > 1 :
+#                 if check and num are the same, use diefferent approach
                 count +=1
                 dic[check] -=2
-                
-            # if num != check:
-            # dic[num] += 1 
         
         return  count 
