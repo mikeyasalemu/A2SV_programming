0335a0141562f00585ec45835fe68be09b918837
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-01 02:01:42 +0300

    Time: 2858 ms (62.04%), Space: 59.2 MB (11.43%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -3,7 +3,6 @@ class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]: 
         m = len(grid)
         n = len(grid[0])
-        answer = np.zeros((m, n), int)
         count_row = np.zeros(m, int)
         count_clmn = np.zeros(n, int)
         
@@ -18,7 +17,7 @@ class Solution:
        
         for row in range(m):
             for clmn in range(n):
-                answer[row][clmn] = count_row[row] + count_clmn[clmn] - (m - count_row[row]) - (n - count_clmn[clmn])
+                grid[row][clmn] = count_row[row] + count_clmn[clmn] - (m - count_row[row]) - (n - count_clmn[clmn])
                
-        return answer
+        return grid
         
