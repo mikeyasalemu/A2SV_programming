3fa59c02b437e4e6fb2b04ee3309152c1e0f844c
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-03 01:25:27 +0300

    Time: 1773 ms (27.72%), Space: 43.7 MB (77.53%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -3,23 +3,23 @@ class Bank:
     def __init__(self, balance: List[int]):
         
         self.balance = balance
-        self.size = len(self.balance)
+
     def transfer(self, account1: int, account2: int, money: int) -> bool:
         
-        if account1 <= self.size and account2 <= self.size and self.balance[account1 -1] >= money:
+        if account1 <= len(self.balance) and account2 <= len(self.balance) and self.balance[account1 -1] >= money:
             self.balance[account1 -1] -= money
             self.balance[account2 -1] += money
             return True
         return False
     def deposit(self, account: int, money: int) -> bool:
-        if account <= self.size:
+        if account <= len(self.balance):
             self.balance[account -1] += money
             return True
         return False
 
     def withdraw(self, account: int, money: int) -> bool:
         
-        if account <= self.size and self.balance[account - 1] >= money:
+        if account <= len(self.balance) and self.balance[account - 1] >= money:
             self.balance[account - 1] -= money
             return True
         return False
