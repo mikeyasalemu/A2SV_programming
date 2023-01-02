523042a87fd2f0ad0e54458e289b08de7d7d322d
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-03 01:31:58 +0300

    Time: 732 ms (82.02%), Space: 43.7 MB (94.01%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -3,26 +3,26 @@ class Bank:
     def __init__(self, balance: List[int]):
         
         self.balance = balance
-
-    def transfer(self, account1: int, account2: int, money: int) -> bool:
         
-        if account1 <= len(self.balance) and account2 <= len(self.balance) and self.balance[account1 -1] >= money:
-            self.balance[account1 -1] -= money
-            self.balance[account2 -1] += money
-            return True
-        return False
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1 > len(self.balance) or account2 > len(self.balance) or self.balance[account1 -1] < money:
+            return False
+       
+        self.balance[account1 -1] -= money
+        self.balance[account2 -1] += money
+        return True
     def deposit(self, account: int, money: int) -> bool:
-        if account <= len(self.balance):
-            self.balance[account -1] += money
-            return True
-        return False
+        if account > len(self.balance):
+            return False
+        self.balance[account -1] += money
+        return True
 
     def withdraw(self, account: int, money: int) -> bool:
         
-        if account <= len(self.balance) and self.balance[account - 1] >= money:
-            self.balance[account - 1] -= money
-            return True
-        return False
+        if account > len(self.balance) or self.balance[account - 1] < money:
+            return False
+        self.balance[account - 1] -= money
+        return True
 
 
 # Your Bank object will be instantiated and called as such:
