e44458cbf8db1fb1dc70cc00325750e39cbe49e1
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-10 18:54:41 +0300

    Time: 68 ms (94.61%), Space: 14.2 MB (50.92%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -6,11 +6,15 @@ class Solution:
         left_inc = 0
         right = 0
         left = 0
+#         count the number of arrays in the right with value 1 and put their index sum 
         for i in range(size):
             if boxes[i] == '1':
                 right += i
                 right_dec +=1
         
+#       add the left and right pointer sum (prefix sum)
+#       when we encounter an array with value 1 , increase the left pointer and decrease the right by 1
+
         for i in range(size):
             summ = 0
             summ = right + left
@@ -18,6 +22,7 @@ class Solution:
             if boxes[i] == '1':
                 right_dec -= 1
                 left_inc += 1
+                
             right -= right_dec 
             left += left_inc
             ans.append(summ)
