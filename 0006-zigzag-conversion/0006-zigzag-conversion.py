1ea51be2e924c29ede3c9c92a2258b9b35fd3db0
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-12 20:06:05 +0300

    Time: 83 ms (61.04%), Space: 14.3 MB (17.64%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,31 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        ans = []
+        for i in range(numRows):
+            ans.append([])
+        ind = 0
+        forward = True
+        
+        for i in range(len(s)):
+            ans[ind].append(s[i])
+            
+            if forward:
+                if ind < numRows-1:
+                    ind +=1
+                else:
+                    forward =False
+                    ind -= 1
+            else:
+                if ind > 0:
+                    ind -= 1
+                else:
+                    forward = True
+                    ind += 1
+        
+        print(ans)
+        res = []
+        for row in ans:
+            res += row
+            
+        return "".join(res)
+        
