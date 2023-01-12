d7cb26a6d64ae311d76833951a75a23c0f56de3c
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-12 18:25:16 +0300

    Time: 33 ms (99.41%), Space: 14 MB (53.69%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,8 +1,10 @@
 class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
-        
+#         put all the possible ways the king can be captured in array
         check = [[-1, 0], [1, 0], [0,-1], [0, 1], [-1, 1], [-1, -1] ,[1, 1], [1, -1]]
         ans = []
+        
+#       typecast for checking in constant time
         queens = set(map(tuple, queens))
         
         for row, column in check:
