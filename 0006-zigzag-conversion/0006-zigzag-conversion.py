62e2c2dff27c46c4115ce7da2fd889e3df502e12
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-12 22:14:52 +0300

    Time: 45 ms (98.85%), Space: 14 MB (75.14%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -3,34 +3,25 @@ class Solution:
         ans = [''] * numRows
         # for i in range(numRows):
         #     ans.append([])
-        if numRows == 1:
-            return s
         ind = 0
         forward = True
         
         for i in range(len(s)):
             ans[ind]+= s[i]
             # print (ans)
-            if ind == numRows -1:
-                forward = False
-            elif ind == 0:
-                forward = True
+           
             if forward:
-                ind += 1
+                if ind < numRows-1:
+                    ind +=1
+                else:
+                    forward =False
+                    ind -= 1
             else:
-                ind -=1
-            # if forward:
-            #     if ind < numRows-1:
-            #         ind +=1
-            #     else:
-            #         forward =False
-            #         ind -= 1
-            # else:
-            #     if ind > 0:
-            #         ind -= 1
-            #     else:
-            #         forward = True
-            #         ind += 1
+                if ind > 0:
+                    ind -= 1
+                else:
+                    forward = True
+                    ind += 1
         # print (ans)
         # res =[]
         # for row in ans:
