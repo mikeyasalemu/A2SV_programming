0b5e3f5efefe8c6a1a7c6423bd4330f8bfa61dd3
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-12 14:44:00 +0300

    Time: 63 ms (64.70%), Space: 13.9 MB (63.90%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        arr = [0]*26
+        for ch in words[0]:
+            arr[ord(ch) -97] += 1
+
+        size = len(words)
+        for index in  range(1,size,1):
+            arr2 = [0]*26
+            for ch in words[index]:
+                arr2[ord(ch) -97] += 1
+            for itr in range(26):
+                arr[itr] = min(arr[itr], arr2[itr])
+                
+        ans = []
+        
+        for index in range(26):
+            for value in range(arr[index]):
+                ans.append(chr(index + 97))
+        return ans
+    
