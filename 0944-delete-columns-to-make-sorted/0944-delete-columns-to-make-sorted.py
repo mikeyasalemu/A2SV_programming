f84e6a046a0102db0470326283b517e1e6a262cf
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-13 17:44:31 +0300

    Time: 73 ms (99.87%), Space: 14.8 MB (23.38%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,18 +1,23 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-            arr = []
-            for i in range(len(strs[0])):
-                    arr.append("")
-            for i in range(len(strs)):
-                for j in range(len(strs[0])):
-                    arr[j]+= strs[i][j]
-            count = 0
-            string1 = ""
-            string1 = ""
-            for i in range(len(arr)):
-                string1 = "".join(sorted(arr[i]))
-                string2 = arr[i]
-                if string1 != string2:
-                    count+=1
+        res = 0
+        for t in zip(*strs):
+            if sorted(t) != list(t):
+                res += 1
+        return res
+#             arr = []
+#             for i in range(len(strs[0])):
+#                     arr.append("")
+#             for i in range(len(strs)):
+#                 for j in range(len(strs[0])):
+#                     arr[j]+= strs[i][j]
+#             count = 0
+#             string1 = ""
+#             string1 = ""
+#             for i in range(len(arr)):
+#                 string1 = "".join(sorted(arr[i]))
+#                 string2 = arr[i]
+#                 if string1 != string2:
+#                     count+=1
                 
-            return count
+#             return count
