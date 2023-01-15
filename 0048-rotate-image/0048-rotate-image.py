0d9397f8a46c127a85bdc4d2a8df0d24c00a0a8f
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-15 22:17:05 +0300

    Time: 35 ms (88.07%), Space: 13.9 MB (27.29%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,18 +5,16 @@ class Solution:
         """
          # n by n matrix 
         side_length = len(matrix) 
-        # can be singular matrix by constraints
         if side_length == 1 : 
             return 
-        # otherwise, we need to do rotations 
-        # loop over half of the side sets, as rotations will accomodate other half automatically. 
+        
         for row in range(int(side_length/2)) : 
-            # column limit of rotation
+          
             col_lim = side_length - row - 1 
-            # loop columns in range 
+           
             for col in range(row, col_lim) :
-                # row limit currently  
+               
                 row_lim = side_length - col - 1
-                # swap four positions each time  
+              
                 matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim], matrix[row_lim][row] = matrix[row_lim][row], matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim]
        
