939bfe546d2430de26709009571420438504b76b
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-15 12:50:15 +0300

    Time: 32 ms (94.18%), Space: 13.8 MB (98.46%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,10 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        matrix.reverse()
-        i = 0
-        for element in (zip(*matrix)):
-            matrix[i] = list(element)
-            i+=1
+         # n by n matrix 
+        side_length = len(matrix) 
+        # can be singular matrix by constraints
+        if side_length == 1 : 
+            return 
+        # otherwise, we need to do rotations 
+        # loop over half of the side sets, as rotations will accomodate other half automatically. 
+        for row in range(int(side_length/2)) : 
+            # column limit of rotation
+            col_lim = side_length - row - 1 
+            # loop columns in range 
+            for col in range(row, col_lim) :
+                # row limit currently  
+                row_lim = side_length - col - 1
+                # swap four positions each time  
+                matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim], matrix[row_lim][row] = matrix[row_lim][row], matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim]
        
