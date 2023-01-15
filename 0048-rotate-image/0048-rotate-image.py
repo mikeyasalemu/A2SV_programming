42dbec6a1f203bb950ab164c4a1208ac559beb33
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-15 22:19:58 +0300

    Time: 36 ms (84.22%), Space: 13.8 MB (70.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,18 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-         # n by n matrix 
-        side_length = len(matrix) 
-        if side_length == 1 : 
-            return 
         
-        for row in range(side_length//2) : 
-          
-            col_lim = side_length - row - 1 
-           
-            for col in range(row, col_lim) :
-               
-                row_lim = side_length - col - 1
-              
-                matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim], matrix[row_lim][row] = matrix[row_lim][row], matrix[row][col], matrix[col][col_lim], matrix[col_lim][row_lim]
-       
+        matrix.reverse()
+        i = 0
+        for element in (zip(*matrix)):
+            matrix[i] = list(element)
+            i+=1
