59f849d5a5e800c9e065e036a4df35e0b6a6450c
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-18 01:28:53 +0300

    Time: 93 ms (74.63%), Space: 14.8 MB (32.18%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -6,14 +6,14 @@ class Solution:
         else:
             ans =[[] for i in range(r)]
             hold = [] * len(mat)
-            for i in range(len(mat)):
-                for j in range(len(mat[0])):
+            row = len(mat)
+            column = len(mat[0])
+            for i in range(row):
+                for j in range(column):
                     hold.append(mat[i][j])  
             index = 0
-       
             for i in range(r):
                 for j in range(c):
                     ans[i].append(hold[index])
-                   
                     index +=1
         return ans
