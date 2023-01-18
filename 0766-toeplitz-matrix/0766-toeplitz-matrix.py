1228acfb24d4eb222a97b1fe42d5073ae596e391
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-19 01:24:30 +0300

    Time: 90 ms (73.76%), Space: 13.8 MB (71.73%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,8 +1,21 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        row,col = len(matrix),len(matrix[0])
-        for r in range(1,row):
-            for c in range(1,col):
-                if matrix[r][c]!=matrix[r-1][c-1]:
-                    return False
-        return True
+          dic = {}
+          check = True
+          # row = len(matrix)
+          # column = len(matrix[0])
+          for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][j] = 100
+                    
+                if dic.get(i -j):
+                    if dic.get(i -j) != matrix[i][j]:
+                        check = False
+                        break
+                else:
+                    dic[i-j] = matrix[i][j]
+            if not check:
+                break
+          return check
+            
