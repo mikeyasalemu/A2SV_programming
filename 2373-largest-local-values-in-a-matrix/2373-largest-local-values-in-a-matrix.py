e1c915ded173422791e252fef313f5a2db4debfb
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-20 01:09:01 +0300

    Time: 241 ms (38.09%), Space: 14.3 MB (81.12%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -4,6 +4,13 @@ class Solution:
         maxLocal = [[0]*(n-2) for _ in range(n-2)]
         for i in range(1, n-1):
             for j in range(1, n-1):
-                maxLocal[i-1][j-1] = max(grid[i-1][j-1:j+2] + grid[i][j-1:j+2] + grid[i+1][j-1:j+2])
+                lst= [[0]* 3 for _ in range(3)]
+                for index1 in range(-1, 2):
+                    for index2 in range(-1, 2):
+                        
+                        lst[index1+1][index2+1] = grid[i + index1][j + index2]
+                
+                maxLocal[i-1][j-1] = max(lst[0] + lst[1] + lst[2])
+               
         return maxLocal
         
