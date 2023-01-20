ac8b43f7bc5c67b7d01f1a98f64ad97f0d40b8f4
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-21 01:10:06 +0300

    Time: 1010 ms (44.52%), Space: 31.9 MB (99.48%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -8,9 +8,9 @@ class Solution:
                 r= i + r
             nums.append(int(r.lstrip('0')))
          nums.sort()
-         # count = 1
-         # for i in range(1,len(nums)):
-         #        if nums[i -1] != nums[i]:
-         #            count += 1
-         return len(set(nums))
+         count = 1
+         for i in range(1,len(nums)):
+                if nums[i -1] != nums[i]:
+                    count += 1
+         return count
          
