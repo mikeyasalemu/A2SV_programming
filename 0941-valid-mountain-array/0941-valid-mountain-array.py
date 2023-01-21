281784f0a6063da3e9b2d180acb48e774696480a
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-21 23:13:01 +0300

    Time: 208 ms (80.76%), Space: 15.3 MB (64.26%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -5,11 +5,11 @@ class Solution:
                 return False
             inc = 0
             dec = size -1
-            while inc < size -2 and arr[inc] < arr[inc + 1]:
+            while inc < size -1 and arr[inc] < arr[inc + 1]:
                 inc +=1
-            while dec > 1 and arr[dec] < arr[dec -1]:
+            while dec > 0 and arr[dec] < arr[dec -1]:
                 dec -= 1
                 
-            # if dec == 0 or inc == size -1:
-            #     return False
+            if dec == 0 or inc == size -1:
+                return False
             return dec == inc
