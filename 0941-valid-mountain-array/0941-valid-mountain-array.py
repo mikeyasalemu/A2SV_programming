688fb37edd396dd9f37890b1b80176c9e5aada1e
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-21 22:54:49 +0300

    Time: 270 ms (36.15%), Space: 15.4 MB (18.99%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -2,8 +2,6 @@ class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
             ans = True
             inc = True
-            if len(arr) == 1:
-                return False
             for i in range(len(arr) -1):
                   if arr[i] == arr[i +1]:
                         ans = False
