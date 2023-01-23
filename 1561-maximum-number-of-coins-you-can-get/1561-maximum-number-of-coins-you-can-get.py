83551d6f814e90e535c8b0ea9f4f89ce91fd6813
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-24 01:32:51 +0300

    Time: 694 ms (45.99%), Space: 26.7 MB (38.89%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        hold = int(len(piles) / 3)
+        ans = 0
+        piles.sort()
+        index = -2
+        i = 0
+        while i < hold:
+            ans+= piles[index]
+            index+= -2
+            i+=1
+            
+        return ans
