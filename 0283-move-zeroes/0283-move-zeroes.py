06d971db82e9a0d42e58da057cd7d15456d41dd4
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-24 18:45:57 +0300

    Time: 236 ms (46.71%), Space: 15.7 MB (12.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,21 +5,14 @@ class Solution:
         """
         size = len(nums)
         values = 0
-        index = values + 1
+
         
-        while index < size:
-            
-            if nums[values] == 0 and nums[index] != 0:
-                nums[values] = nums[index]
-                nums[index] = 0
-                values +=1
-                index +=1
-            elif nums[values] == 0 and nums[index] == 0:
-                index +=1
-            else:
-                values +=1
-                index +=1
-                
+        for  index in range(size):
+            if nums[index] != 0:
+                nums[values], nums[index] = nums[index], nums[values]
+                values+=1
+         
+           
         return nums
             
             
