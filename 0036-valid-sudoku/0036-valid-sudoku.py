7639e6ec527e48111e4ce31189c471cf78c75478
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-25 18:20:19 +0300

    Time: 112 ms (41.12%), Space: 13.8 MB (76.92%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,31 +2,31 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         check = True
         for i in range(9):
-            dic = {}
+            dic_column = {}
             for j in range(9):
-                if dic.get(board[i][j]):
+                if dic_column.get(board[i][j]):
                     return False
                 elif board[i][j] != ".":
-                    dic[board[i][j]] = 1
+                    dic_column[board[i][j]] = 1
                     
         if check:
             for i in range(9):
-                dic = {}
+                dic_row = {}
                 for j in range(9):
-                    if dic.get(board[j][i]):
+                    if dic_row.get(board[j][i]):
                         return False
                     elif board[j][i] != ".":
-                        dic[board[j][i]] = 1
+                        dic_row[board[j][i]] = 1
         if check:
             for i in range(0, 9, 3):
                 for j in range(0, 9, 3):
-                    dic = {}
+                    dic_box = {}
                     for x in range(i, i+3):
                         for y in range(j, j+3):
-                                if dic.get(board[x][y]):
+                                if dic_box.get(board[x][y]):
                                     return False
                                 elif board[x][y] != ".":
-                                    dic[board[x][y]] = 1
+                                    dic_box[board[x][y]] = 1
                 
         return check
         
