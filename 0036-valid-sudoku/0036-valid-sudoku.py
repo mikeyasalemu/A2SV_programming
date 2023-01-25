dc6b338bcffd3711f166506cf82f62967625860c
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-25 19:12:04 +0300

    Time: 99 ms (73.15%), Space: 13.9 MB (29.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,31 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = collections.defaultdict(set)
-        col = collections.defaultdict(set)
-        sq = collections.defaultdict(set)
+        check = True
         for i in range(9):
+            dic = {}
             for j in range(9):
-                cur = board[i][j]
-                if cur == '.':
-                    continue
-                if cur in row[i] or cur in col[j] or cur in sq[(i//3,j//3)]:
+                if dic.get(board[i][j]):
                     return False
-                else:
-                    row[i].add(cur)
-                    col[j].add(cur)
-                    sq[(i//3,j//3)].add(cur)
-        return True
+                elif board[i][j] != ".":
+                    dic[board[i][j]] = 1
+                    
+        if check:
+            for i in range(9):
+                dic = {}
+                for j in range(9):
+                    if dic.get(board[j][i]):
+                        return False
+                    elif board[j][i] != ".":
+                        dic[board[j][i]] = 1
+        if check:
+            for i in range(0, 9, 3):
+                for j in range(0, 9, 3):
+                    dic = {}
+                    for x in range(i, i+3):
+                        for y in range(j, j+3):
+                                if dic.get(board[x][y]):
+                                    return False
+                                elif board[x][y] != ".":
+                                    dic[board[x][y]] = 1
+                
+        return check
