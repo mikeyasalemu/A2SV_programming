ec8b4ae7618927165a253cb4f043e3faf9ebc1f2
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-25 19:07:13 +0300

    Time: 211 ms (22.90%), Space: 14 MB (5.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        #Column check
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        sq = collections.defaultdict(set)
         for i in range(9):
-            dic_column = {}
             for j in range(9):
-                if dic_column.get(board[i][j]):
+                cur = board[i][j]
+                if cur == '.':
+                    continue
+                if cur in row[i] or cur in col[j] or cur in sq[(i//3,j//3)]:
                     return False
-                elif board[i][j] != ".":
-                    dic_column[board[i][j]] = 1
-        #row check      
-        for i in range(9):
-            dic_row = {}
-            for j in range(9):
-                if dic_row.get(board[j][i]):
-                    return False
-                elif board[j][i] != ".":
-                    dic_row[board[j][i]] = 1
-        
-        #box check
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                dic_box = {}
-                for x in range(i, i+3):
-                    for y in range(j, j+3):
-                            if dic_box.get(board[x][y]):
-                                return False
-                            elif board[x][y] != ".":
-                                dic_box[board[x][y]] = 1
-                
+                else:
+                    row[i].add(cur)
+                    print(row)
+                    col[j].add(cur)
+                    sq[(i//3,j//3)].add(cur)
         return True
-        
-       
-        
