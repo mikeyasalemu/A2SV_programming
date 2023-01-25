0a2105400956e52cb9ce74330e81e5432d0b82cb
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-25 18:21:55 +0300

    Time: 139 ms (32.82%), Space: 14 MB (29.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,6 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         check = True
+        #Column check
         for i in range(9):
             dic_column = {}
             for j in range(9):
@@ -8,25 +9,25 @@ class Solution:
                     return False
                 elif board[i][j] != ".":
                     dic_column[board[i][j]] = 1
-                    
-        if check:
-            for i in range(9):
-                dic_row = {}
-                for j in range(9):
-                    if dic_row.get(board[j][i]):
-                        return False
-                    elif board[j][i] != ".":
-                        dic_row[board[j][i]] = 1
-        if check:
-            for i in range(0, 9, 3):
-                for j in range(0, 9, 3):
-                    dic_box = {}
-                    for x in range(i, i+3):
-                        for y in range(j, j+3):
-                                if dic_box.get(board[x][y]):
-                                    return False
-                                elif board[x][y] != ".":
-                                    dic_box[board[x][y]] = 1
+        #row check      
+        for i in range(9):
+            dic_row = {}
+            for j in range(9):
+                if dic_row.get(board[j][i]):
+                    return False
+                elif board[j][i] != ".":
+                    dic_row[board[j][i]] = 1
+        
+        #box check
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                dic_box = {}
+                for x in range(i, i+3):
+                    for y in range(j, j+3):
+                            if dic_box.get(board[x][y]):
+                                return False
+                            elif board[x][y] != ".":
+                                dic_box[board[x][y]] = 1
                 
         return check
         
