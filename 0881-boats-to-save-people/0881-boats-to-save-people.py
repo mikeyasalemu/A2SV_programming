9afdc5fda4c7e7d15375fa0a19b4401123241b88
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-01-30 01:34:31 +0300

    Time: 471 ms (77.38%), Space: 20.9 MB (66.07%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,8 +1,6 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        if len(people) == 1:
-            return 1
         ans =  0
         i = 0
         j = len(people) -1
