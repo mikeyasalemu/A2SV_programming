4f4783a1d30054278ca8951c9acde5dc369cbd79
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-01 19:41:45 +0300

    Time: 2159 ms (16.51%), Space: 25.4 MB (76.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        k = k % len(nums)
         for i in range(k):
             num=  nums.pop()
             nums.insert(0, num)
