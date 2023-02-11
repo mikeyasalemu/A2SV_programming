499b178e7650f48d480006d11908f0055d41062f
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-12 00:51:05 +0300

    Time: 59 ms (96.64%), Space: 14 MB (7.90%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,70 +5,83 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        check = "equal"
+        check = 0
         ans = ListNode(0)
         start = ans
         rem = 0
         
-        while l1 and l2:
+        while l1 or l2:
             num = l1.val + l2.val + rem
-            
             rem = 0
             if num > 9:
                 rem = 1
                 num -= 10
-            # print (num, rem)
             ans.val = num
-            if l1.next is None and l2.next is None:
+            
+            if not l1.next and not l2.next:
                 break
-            elif  l1.next is None:
-                check = "one"
-                break
-            elif  l2.next is None:
-                check =  "two"
-                break 
             else:
-                l1 = l1.next
-                l2 = l2.next
                 temp = ListNode(0)
                 ans.next = temp
                 ans = ans.next
-        # print (start, check)
-        if check == "two":
-            temp = ListNode(0)
-            ans.next = temp
-            ans = ans.next
-            while l1.next:
+            if l1.next:
                 l1 = l1.next
-                num = rem + l1.val
-                rem = 0
-                if num > 9:
-                    rem = 1
-                    num -= 10
-                ans.val = num
-                if l1.next:
-                    temp = ListNode(0)
-                    ans.next = temp
-                    ans = ans.next
-        elif check == "one":
-            temp = ListNode(0)
-            ans.next = temp
-            ans = ans.next
-            while l2.next:
+            else:
+                l1.val = 0
+            if l2.next:
                 l2 = l2.next
-                num = rem + l2.val
-                
-                rem = 0
-                if num > 9:
-                    rem = 1
-                    num -= 10
-                ans.val = num
-                if l2.next:
-                    temp = ListNode(0)
-                    ans.next = temp
-                    ans = ans.next
+            else:
+                l2.val = 0
+            
         if rem > 0:
             temp = ListNode(1)
             ans.next = temp
             ans = ans.next
         return  start
+      # if l1.next is None and l2.next is None:
+            #     break
+            # elif  l1.next is None:
+            #     check = "one"
+            #     break
+            # elif  l2.next is None:
+            #     check =  "two"
+            #     break 
+            # else:
+            #     l1 = l1.next
+            #     l2 = l2.next
+            #     temp = ListNode(0)
+            #     ans.next = temp
+            #     ans = ans.next
+#         if check == "two":
+#             temp = ListNode(0)
+#             ans.next = temp
+#             ans = ans.next
+#             while l1.next:
+#                 l1 = l1.next
+#                 num = rem + l1.val
+#                 rem = 0
+#                 if num > 9:
+#                     rem = 1
+#                     num -= 10
+#                 ans.val = num
+#                 if l1.next:
+#                     temp = ListNode(0)
+#                     ans.next = temp
+#                     ans = ans.next
+#         elif check == "one":
+#             temp = ListNode(0)
+#             ans.next = temp
+#             ans = ans.next
+#             while l2.next:
+#                 l2 = l2.next
+#                 num = rem + l2.val
+                
+#                 rem = 0
+#                 if num > 9:
+#                     rem = 1
+#                     num -= 10
+#                 ans.val = num
+#                 if l2.next:
+#                     temp = ListNode(0)
+#                     ans.next = temp
+#                     ans = ans.next
