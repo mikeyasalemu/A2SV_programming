f9739c52b93eca6a61512615c2ff93620ede2035
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-12 00:53:12 +0300

    Time: 57 ms (98.15%), Space: 14 MB (35.70%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -9,8 +9,7 @@ class Solution:
         ans = ListNode(0)
         start = ans
         rem = 0
-        
-        while l1 or l2:
+        while l1 or l2 or rem > 0:
             num = l1.val + l2.val + rem
             rem = 0
             if num > 9:
@@ -18,7 +17,7 @@ class Solution:
                 num -= 10
             ans.val = num
             
-            if not l1.next and not l2.next:
+            if not l1.next and not l2.next and rem == 0:
                 break
             else:
                 temp = ListNode(0)
@@ -33,55 +32,9 @@ class Solution:
             else:
                 l2.val = 0
             
-        if rem > 0:
-            temp = ListNode(1)
-            ans.next = temp
-            ans = ans.next
+        # if rem > 0:
+        #     temp = ListNode(1)
+        #     ans.next = temp
+        #     ans = ans.next
         return  start
-      # if l1.next is None and l2.next is None:
-            #     break
-            # elif  l1.next is None:
-            #     check = "one"
-            #     break
-            # elif  l2.next is None:
-            #     check =  "two"
-            #     break 
-            # else:
-            #     l1 = l1.next
-            #     l2 = l2.next
-            #     temp = ListNode(0)
-            #     ans.next = temp
-            #     ans = ans.next
-#         if check == "two":
-#             temp = ListNode(0)
-#             ans.next = temp
-#             ans = ans.next
-#             while l1.next:
-#                 l1 = l1.next
-#                 num = rem + l1.val
-#                 rem = 0
-#                 if num > 9:
-#                     rem = 1
-#                     num -= 10
-#                 ans.val = num
-#                 if l1.next:
-#                     temp = ListNode(0)
-#                     ans.next = temp
-#                     ans = ans.next
-#         elif check == "one":
-#             temp = ListNode(0)
-#             ans.next = temp
-#             ans = ans.next
-#             while l2.next:
-#                 l2 = l2.next
-#                 num = rem + l2.val
-                
-#                 rem = 0
-#                 if num > 9:
-#                     rem = 1
-#                     num -= 10
-#                 ans.val = num
-#                 if l2.next:
-#                     temp = ListNode(0)
-#                     ans.next = temp
-#                     ans = ans.next
+    
