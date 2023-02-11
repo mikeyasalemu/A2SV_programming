4c6837360e9b5d2dea8a75782bb0a7ae5df02bbd
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-12 00:54:37 +0300

    Time: 70 ms (66.66%), Space: 14 MB (7.90%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,7 +5,6 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        check = 0
         ans = ListNode(0)
         start = ans
         rem = 0
