3b3c78e122b9713b1c8eb380ab317407447e76e0
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-23 10:01:49 +0300

    Time: 1428 ms (22.68%), Space: 26.2 MB (14.19%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -6,14 +6,16 @@ class Solution:
         ans =0.0
         if k > len(nums):
             return 0
-        while right < k:
-            ans += nums[right]
-            right +=1
-        avg = ans
+       
         while right < len(nums):
-            avg = avg - nums[left] + nums[right]
-            ans = max(ans, avg)
-            right +=1
-            left +=1
+            if right < k:
+                ans += nums[right]
+                right +=1
+                avg = ans
+            else:
+                avg = avg - nums[left] + nums[right]
+                ans = max(ans, avg)
+                right +=1
+                left +=1
             
         return ans/k
