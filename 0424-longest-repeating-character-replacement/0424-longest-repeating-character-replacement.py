e997c2264cc9f156ec61f1bd1c1eea72d058a1fb
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-02-23 16:19:55 +0300

    Time: 375 ms (14.46%), Space: 13.9 MB (90.68%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        right = 0
+        left = 0
+        length = len(s)
+        sets= set(s)
+        ans = 0
+        for ch in sets:
+            diff = 0
+            left = 0
+            right = 0
+            while right < length:
+                 if ch != s[right]:
+                        diff+=1
+                 if diff <= k:
+                    ans = max(ans, right - left +1)
+                 else:
+                    if s[left] != ch:
+                        diff -= 1
+                    left+=1
+                            
+                 right +=1
+        return ans
+                
