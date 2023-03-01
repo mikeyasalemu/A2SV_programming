61ff08a36d677f5e151017f4174ff9c5aaa3d37f
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-01 18:18:06 +0300

    Time: 4764 ms (12.00%), Space: 13.8 MB (98.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,33 +1,16 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        score = self.getScore(nums,0,len(nums)-1,True)
-        if score[0] >= score[1]:
+        score = self.getScore(nums,0,len(nums)-1)
+        if score >= 0:
             return True
         return False
 
-    def getScore(self,arr,l,r,p1t):
+    def getScore(self,arr,l,r):
         
         if l == r:
-            if p1t:
-                return [arr[l],0]
-            else:
-                return [0,arr[l]]
+            ret = arr[l]
+            return ret
+        return max(arr[l] - self.getScore(arr, l +1, r), arr[r] - self.getScore(arr, l, r -1))
+          
         
-        if p1t:
-            left = self.getScore(arr,l+1,r,not p1t)
-            right = self.getScore(arr,l,r-1,not p1t)
-            left[0] += arr[l]
-            right[0] += arr[r]
-            if left[0] >= right[0]:
-                return left
-            else:
-                return right
-        else:
-            left = self.getScore(arr,l+1,r,not p1t)
-            right = self.getScore(arr,l,r-1,not p1t)
-            left[1] += arr[l]
-            right[1] += arr[r]
-            if left[1] > right[1]:
-                return left
-            else:
-                return right
+       
