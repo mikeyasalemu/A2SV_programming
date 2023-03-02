d661fd721fbfd0b0003104c9398e4985702c1bd4
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-02 22:19:15 +0300

    Time: 42 ms (29.33%), Space: 13.8 MB (98.18%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -4,21 +4,15 @@ class Solution:
         ans = []
         # print(path)
         for i in range(len(path)):
-            if path[i] == "":
-                if not ans or ans[-1]!='/':
-                    ans.append('/')
+            if path[i] == "" or path[i] == ".":
+                continue
             elif path[i] == "..":
                     if ans:
-                        while ans and ans[-1] == "/" and len(ans) > 2:
-                                  ans.pop()
-                        if ans and len(ans) > 1:
-                            ans.pop()
+                        ans.pop()
             elif path[i] == '.':
                  continue
             else:
-                 ans.append(path[i])
-                 ans.append('/')  
-        while len(ans) > 2 and ans[-1] == '/':
-            ans.pop()
-            
-        return "".join(ans)
+                 ans.append(path[i]) 
+        ret = '/'.join(ans)
+        ret = '/' + ret
+        return ret
