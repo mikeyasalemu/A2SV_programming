20d736731f4e0ce0676c083179cc15122733613c
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-03 13:29:45 +0300

    Time: 39 ms (22.86%), Space: 13.9 MB (49.69%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        ans = 0
+        ans = []
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                ans.append(s[i])
+            else:
+                temp = 1
+                if not ans[-1] == '(':
+                    temp = ans.pop() * 2
+                ans.pop()
+                if ans and not ans[-1] == '(':
+                    ans[-1] += temp
+                else:
+                    ans.append(temp)
+                
+        
+        return ans[-1]
+                
+        
