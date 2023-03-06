041704144d23b3806bc24ee0e59014127fac0aef
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-06 16:50:24 +0300

    Time: 53 ms (57.86%), Space: 14.7 MB (30.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -4,10 +4,12 @@ class Solution:
         high = len(nums) -1
         while low <= high:
             mid = low + (high - low)//2
-            if nums[mid] < target:
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
                 low = mid +1
             elif nums[mid] > target:
                 high = mid -1
-            else:
-                return mid
+            
         return low
