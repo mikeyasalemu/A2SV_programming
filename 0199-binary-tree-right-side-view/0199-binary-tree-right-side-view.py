2730aa1fef575510903a8b6b198f435c71339f37
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-10 14:48:00 +0300

    Time: 39 ms (31.79%), Space: 13.9 MB (15.16%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,10 +7,11 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         ans = []
-        self.dic = defaultdict(list)
+        self.dic = defaultdict(int)
         self.helper(root,0)
         for lst in sorted(self.dic):
-            ans.append(self.dic[lst][-1])
+            ans.append(self.dic[lst])
+        print(self.dic)
         return ans
 
     def helper(self,root,count):
@@ -18,8 +19,9 @@ class Solution:
             return
 
         self.helper(root.left,count+1)
+        self.dic[count] = root.val
         self.helper(root.right,count+1)
-        self.dic[count].append(root.val)
+        
       
 
         
