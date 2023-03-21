8783dd3e63f7b995c6cc31888750e0d8f6f83743
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-21 18:27:44 +0300

    Time: 49 ms (51.79%), Space: 18.6 MB (16.03%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -7,20 +7,19 @@
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.dic = defaultdict(int)
-        self.dict = defaultdict(list)
-        self.ans = 0
-        self.helper(root,1,0)
-        # print(self.dic)
-        # print(self.dict)
+        self.dict = defaultdict(int)
+        self.ans = 1
+        self.helper(root,1,1)
         return self.ans
     def helper(self,root,count,level):
-        # self.dic[count]+=1
+        
         if not root: 
             return     
-        # self.dict[count].append(self.dic[count])
-        # self.ans = max(self.ans, (max(self.dict[count]) -min(self.dict[count])) +1)
-        self.dict[level].append(count)
-        self.ans = max(self.ans, (max(self.dict[level]) -min(self.dict[level])) +1)
+        if self.dict[level] > 0:
+            self.ans = max(self.ans, abs(self.dict[level] - count) +1)
+        else:
+            self.dict[level] = count
+            # self.ans = max(self.ans, count)
         self.helper(root.left,(count*2) -1,level+1)
         self.helper(root.right,(count*2),level+1)
             
