99195d5ba47b2aef7348b6037d5b1bacd0fa228b
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-22 11:02:52 +0300

    Time: 88 ms (63.07%), Space: 14.7 MB (13.32%) - LeetHub

---
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -0,0 +1,41 @@
+class Solution:
+    def numSmallerByFrequency(self, queries: List[str], words: List[str]) -> List[int]:
+        for i in range(len(queries)):
+            queries[i] = ''.join(sorted(queries[i]))
+        for i in range(len(words)):
+            words[i] = ''.join(sorted(words[i]))
+        q = []
+        w = []
+        for i in range(len(queries)):
+            temp = queries[i][0]
+            freq = 1
+            for j in range(1,len(queries[i])):
+                if queries[i][j] == temp:
+                    freq+=1
+                else:
+                    break
+            q.append(freq)
+        for i in range(len(words)):
+            temp = words[i][0]
+            freq = 1
+            for j in range(1,len(words[i])):
+                if words[i][j] == temp:
+                    freq+=1
+                else:
+                    break
+            w.append(freq)
+        w.sort()
+        print(q)
+        print(w)
+        ans = []
+        for i in range(len(q)):
+            left = 0
+            right = len(w) -1
+            while left <= right:
+                mid = left + (right - left)//2
+                if w[mid] > q[i]:
+                    right = mid -1
+                else:
+                    left = mid+1
+            ans.append(len(w) - left)
+        return ans
