7971812e2d2af9806421096c84dfb23eb0d444f0
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-23 19:52:26 +0300

    Time: 407 ms (23.20%), Space: 32 MB (94.33%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -4,10 +4,8 @@ class Solution:
         stack =[]
         for i in range(len(nums)):
             low = min(nums[i],low)
-            if not stack or stack[-1][0] < nums[i]:
-                while stack and stack[-1][0] < nums[i]:
-                    stack.pop()
-                
+            while stack and stack[-1][0] < nums[i]:
+                stack.pop()
             if stack and stack[-1][1] < stack[-1][0] > nums[i] and stack[-1][1] < nums[i]:
                 return True
             stack.append([nums[i],low])
