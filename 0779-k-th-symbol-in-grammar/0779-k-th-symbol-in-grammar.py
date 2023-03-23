99157d206a13f1fd5b2a215eea67d01a71bab435
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-24 01:57:25 +0300

    Time: 34 ms (47.96%), Space: 13.9 MB (10.18%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,34 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        if n == 2:
+            if k == 1:
+                return 0
+            return 1
+        check = self.helper(1, 1,n,k)
+        # print(check)
+        if check[1] == True:
+            return 1
+        return 0
+    def helper(self,size,count,n,k):
+        if count == n:
+            if k > (size//2):
+                return [abs((size//2) - k), True]
+            else:
+                return [k, False]
+            
+        temp = self.helper(size*2,count+1,n,k)
+        # print(count,temp)
+        if size > 1:
+            if temp[0] > (size//2):
+                    tr = True
+                    if temp[1] == True:
+                        tr = False
+                    return [abs((size//2) - temp[0]), tr]
+            else:
+                tr = False
+                if temp[1] == True:
+                        tr = True
+                return [temp[0] , tr]
+        return [temp[0],temp[1]]
