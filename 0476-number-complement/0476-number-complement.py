aeb568e7e910bed3870f55ba567b171bd099fb28
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-30 18:00:37 +0300

    Time: 26 ms (91.15%), Space: 13.7 MB (91.06%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -2,7 +2,6 @@ class Solution:
     def findComplement(self, num: int) -> int:
         temp = 0
         ans = 0
-        # print(2<<0)
         count = 0
         while num > 0:
             if not num&1:
@@ -12,6 +11,4 @@ class Solution:
                 temp = 0
             count+=1
             num = num>>1
-        # print(ans)
-        # print(ans)
         return ans
