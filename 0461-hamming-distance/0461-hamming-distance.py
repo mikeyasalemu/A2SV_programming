a339af6963ce3a1073052317754baedd38bcf0c8
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-03-30 19:25:44 +0300

    Time: 33 ms (50.67%), Space: 13.9 MB (6.64%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,5 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        temp = x^y
-        return temp.bit_count()
+          distance = 0
+          while x > 0 or y > 0:
+            if x & 1 != y & 1:
+              distance += 1
+            x >>= 1
+            y >>= 1
+          return distance
         
