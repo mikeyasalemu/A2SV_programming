d3650aca652987d562e207a4d783a66f59f94ce1
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-04 00:57:19 +0300

    Time: 228 ms (26.00%), Space: 13.8 MB (98.75%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -5,11 +5,8 @@ class Solution:
         return self.ans
     def helper(self, arr, ind, curr):
         self.ans = max(self.ans, len(curr))
-        # print(self.ans,curr)
         for i in range(ind, len(arr)):
             temp = curr+arr[i]
-            # print(temp)
             freq = Counter(temp)
-            # print(freq)
             if len(freq) == (len(curr)+len(arr[i])):
                 self.helper(arr, i+1, temp)
