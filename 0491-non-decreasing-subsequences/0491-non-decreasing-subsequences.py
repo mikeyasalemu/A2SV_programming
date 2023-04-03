7de3bc94958f42429e805d86dfe61d747cfe8345
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-04 00:32:39 +0300

    Time: 7378 ms (5.15%), Space: 25.6 MB (12.12%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,14 +1,14 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
         self.ans = []
-        self.check = set()
+        self.check = []
         self.helper(0,nums,[])
         # print(self.check)
         return self.ans
     def helper(self,state,nums,curr):
-        if len(curr) > 1 and tuple(curr) not in self.check:
+        if len(curr) > 1 and curr not in self.check:
             self.ans.append(curr)
-            self.check.add(tuple(curr))
+            self.check.append(curr[:])
         i = state
         while i < len(nums):
             if not curr or i > 0  and curr and nums[i] >= curr[-1]:
