23150cbca56241ebbd9cd095e171554e76704eb8
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-05 16:04:16 +0300

    Time: 36 ms (56.74%), Space: 14.1 MB (26.29%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,25 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
         self.ch = []
-        self.helper(0,nums,[],0)
-        # print(1<<0)
-        # print(bin(2))
-        print(self.ch)
+        self.helper(0,nums,0)
         return self.ch
     
-    def helper(self,state,nums,curr,ch):
-        self.ans.append(curr)
-        
+    def helper(self,state,nums,ch):        
         self.ch.append([])
         string = bin(ch)
-        
         for i in range(len(string)-1,1,-1):
             if string[i] == '1':
                 self.ch[-1].append(nums[len(string)-1-i])
-        print(ch,string,self.ch[-1])
         for i in range(state,len(nums)):
             temp = 1
             temp = temp<<i
-            self.helper(i+1,nums,curr+[nums[i]],ch|temp)
+            self.helper(i+1,nums,ch|temp)
         
