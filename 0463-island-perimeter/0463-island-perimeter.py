742a6ddc1512508fed8a8d34692a81e1f601859f
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-07 19:48:33 +0300

    Time: 663 ms (20.14%), Space: 25.5 MB (15.13%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,30 +2,24 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
-        self.ans = 0
+        visited = set()
         def inbound(row, col):
             return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
-        def dfs(grid, visited, row, col):
-             # base case
-             # nonlocal ans
+        def dfs(row, col):
              if not inbound(row, col) or grid[row][col] == 0:
-                    self.ans+=1
-                    return
+                    return 1
             
-             if visited[row][col]:
-                return
-            
-             visited[row][col] = True
+             visited.add((row, col))
+             count = 0
              for row_change, col_change in directions:
                  new_row = row + row_change
                  new_col = col + col_change
-                 dfs(grid, visited, new_row, new_col)
-                 # print(ans)
+                 if(new_row, new_col) not in visited:
+                    count += dfs(new_row, new_col)
+             return count
             
              
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    dfs(grid,visited,i,j)
-                    return self.ans
+                   return dfs(i,j)
