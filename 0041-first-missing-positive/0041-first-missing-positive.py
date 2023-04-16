f039f367c5376b4fad33db250fb56a2bc40acb19
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-16 23:33:18 +0300

    Time: 436 ms (17.25%), Space: 27.8 MB (60.88%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -6,10 +6,10 @@ class Solution:
         for idx in range(len(nums)):
             if nums[idx] < 0 or nums[idx] >= len(nums):
                 nums[idx] = 0
-        print(nums)
+    
         for idx in range(len(nums)):
             nums[nums[idx] % n] += n
-        print(nums)   
+        
         for idx in range(1,len(nums)):
             if nums[idx] // n == 0:
                 return idx
