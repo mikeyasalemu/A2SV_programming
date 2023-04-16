2da2a7be69a93db5af501d750ee9a05c18cae7a6
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-16 22:31:28 +0300

    Time: 425 ms (9.85%), Space: 21.1 MB (89.69%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -16,23 +16,3 @@ class Solution:
                 ans.append(nums[ind])
             ind +=1
         return ans
-#         n = len(nums)
-#         indx = 0
-#         ans = []
-#         while indx < len(nums):
-#             postionOfValue = nums[indx] - 1
-#             if postionOfValue != indx:
-#                 if nums[postionOfValue] != nums[indx]:
-#                     nums[postionOfValue], nums[indx] = nums[indx], nums[postionOfValue]
-#                     continue
-#             indx+=1
-#         i = 0
-#         while i < n:
-#              correct_position = nums[i] - 1
-#              if correct_position != i:
-#                     ans.append(nums[i])
-#              i+=1
-            
-            
-#         return ans
-        
