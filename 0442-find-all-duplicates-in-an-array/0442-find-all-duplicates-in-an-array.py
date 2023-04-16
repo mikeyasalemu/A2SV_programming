1db64bf03b78765aa26e19a67ab641c56f9d52c0
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-16 22:31:11 +0300

    Time: 409 ms (16.60%), Space: 21.2 MB (89.69%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,23 +1,38 @@
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        indx = 0
+        ind = 0
         ans = []
-        while indx < len(nums):
-            postionOfValue = nums[indx] - 1
-            if postionOfValue != indx:
-                if nums[postionOfValue] != nums[indx]:
-                    nums[postionOfValue], nums[indx] = nums[indx], nums[postionOfValue]
+        while ind < len(nums):
+            val = nums[ind]-1
+            if val != ind:
+                if nums[val] != nums[ind]:
+                    nums[val],nums[ind] = nums[ind],nums[val]
                     continue
-            indx+=1
-        i = 0
-        print(nums)
-        while i < n:
-             correct_position = nums[i] - 1
-             if correct_position != i:
-                    ans.append(nums[i])
-             i+=1
-            
-            
+            ind+=1
+        ind = 0
+        while ind < len(nums):
+            val = nums[ind] -1
+            if val != ind:
+                ans.append(nums[ind])
+            ind +=1
         return ans
+#         n = len(nums)
+#         indx = 0
+#         ans = []
+#         while indx < len(nums):
+#             postionOfValue = nums[indx] - 1
+#             if postionOfValue != indx:
+#                 if nums[postionOfValue] != nums[indx]:
+#                     nums[postionOfValue], nums[indx] = nums[indx], nums[postionOfValue]
+#                     continue
+#             indx+=1
+#         i = 0
+#         while i < n:
+#              correct_position = nums[i] - 1
+#              if correct_position != i:
+#                     ans.append(nums[i])
+#              i+=1
+            
+            
+#         return ans
         
