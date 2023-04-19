327fc0e25f16b4ba79cbda1869001c030a5ce7e3
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-19 21:58:14 +0300

    Time: 33 ms (89.75%), Space: 13.9 MB (39.70%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -7,14 +7,20 @@
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         root = None
+        
         for val in preorder:
             root = self.BST(root,val)
+            
         return root
+    
+    
     def BST(self,root,val):
         if not root:
             return TreeNode(val,None,None)
+        
         if root.val < val:
             root.right = self.BST(root.right,val)
+            
         else:
             root.left = self.BST(root.left,val)
         
