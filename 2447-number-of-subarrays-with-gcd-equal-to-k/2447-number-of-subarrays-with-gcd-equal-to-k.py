1c06d7b62d6aed15defc065bf7b4525b4e4f5cb4
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-22 00:46:52 +0300

    Time: 241 ms (67.02%), Space: 14 MB (71.31%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
@@ -12,8 +12,3 @@ class Solution:
                     count += 1
                 j += 1
         return count
-        
-    # def GCD(self,a,b):
-    #     if b == 0:
-    #         return a
-    #     return self.GCD(b,a%b)
