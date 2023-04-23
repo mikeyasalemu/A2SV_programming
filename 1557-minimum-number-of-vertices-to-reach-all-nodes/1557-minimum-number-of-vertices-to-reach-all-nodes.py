356288c020050b8aa51c8960d151a5ca48813542
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-23 21:58:06 +0300

    Time: 1244 ms (24.76%), Space: 52.5 MB (49.05%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -3,11 +3,9 @@ class Solution:
         dic = defaultdict(list)
         for node1 , node2 in edges:
             dic[node2].append(node1)
-        print(dic)
         ans = []
         for lst in range(n):
             if len(dic[lst]) == 0:
-                # print(dic[lst])
                 ans.append(lst)
                            
         return ans
