c16a22cca24262e48d369847b6b1338076c6555e
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-27 17:52:00 +0300

    Time: 73 ms (22.35%), Space: 16.9 MB (10.24%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -2,10 +2,7 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         self.visited = set()
         self.visited.add(0)
-        # self.check = [-1]*len(rooms)
-        # self.check[0] = 1
         self.bfs(rooms[0],rooms)
-        print(self.visited)
         return len(self.visited) == len(rooms)
     def bfs(self,room,rooms):
         childs = []
