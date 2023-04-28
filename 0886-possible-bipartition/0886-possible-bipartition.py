3bed814eed3847d78a8b6ac60eac7a631828a7ce
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-04-28 17:52:48 +0300

    Time: 745 ms (27.13%), Space: 23 MB (9.97%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,34 +1,34 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        self.dic = defaultdict(list)
-        self.visited = set()
+        dic = defaultdict(list)
+        visited = set()
         
         for node1,node2 in dislikes:
-            self.dic[node1].append(node2)
-            self.dic[node2].append(node1)
-        self.values = self.dic.values()
+            dic[node1].append(node2)
+            dic[node2].append(node1)
+            
         self.check = [-1] * n
         self.ans = True
         if n == 1:
             return True
-        for val in self.dic.keys():
-            if val not in self.visited and self.ans:
-                self.dfs(val)
+        for val in dic.keys():
+            if val not in visited and self.ans:
+                self.dfs(val,visited,dic)
         return self.ans
-    def dfs(self,node):
-        self.visited.add(node)
-        for val in self.dic[node]:
+    def dfs(self,node,visited,dic):
+        visited.add(node)
+        for val in dic[node]:
             if not self.check:
                 return 
-            if val in self.visited:
+            if val in visited:
                 if self.check[val - 1] == self.check[node-1]:
                     self.ans = False
                     return 
                 else:
                     continue
-            if val not in self.visited:
+            if val not in visited:
                 self.check[val-1] = self.check[node-1]*-1
-                self.dfs(val)
+                self.dfs(val,visited,dic)
         
         
             
