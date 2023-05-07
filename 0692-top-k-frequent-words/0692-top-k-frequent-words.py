ed401fc25a0a79021e0a507a99b58c44216e4723
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-05-07 15:11:59 +0300

    Time: 78 ms (7.83%), Space: 16.3 MB (11.99%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -3,37 +3,11 @@ class Solution:
         dic = Counter(words)
         ans = []
         for key, val in dic.items():
-            ans.append((val,key))
-        dic2 = defaultdict(list)
-        for key,val in dic.items():
-            dic2[val].append(key)
-        dic = OrderedDict(sorted(dic2.items(), reverse=True, key=lambda t: t[0]))
-        ans  =[]
-        for key,val in dic.items():
-            heapify(val)
-            while val and k > 0:
-                ans.append(heappop(val))
-                k-=1
-            if k == 0:
-                break
-        # print(ans)
-        return ans
-#         heapify(ans)
-#         ret = nlargest(k,ans)
-#         heapify(ret)
-#         ans = []
-#         dic = defaultdict(list)
+            ans.append((-1*val,key))
+        heapify(ans)
+        ret = []
+        for i in range(k):
+            ret.append(heappop(ans)[-1])
+        # print(ret)
         
-#         while ret:
-#             freq , key = heappop(ret)
-#             dic[freq].append(key)
-            
-#         dic = OrderedDict(sorted(dic.items(), reverse=True, key=lambda t: t[0]))
-        
-#         for key,val in dic.items():
-#             heapify(val)
-#             while val:
-#                 ans.append(heappop(val))
-        # print(ans)
-        # # print(nlargest(k,ans,key = lambda x:(x[0],x[1])))
-        # return ans
+        return ret
