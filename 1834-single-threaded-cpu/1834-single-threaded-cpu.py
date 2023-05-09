d6117b1a9c2e8d0d8ed17fe3e4ca4da941881540
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-05-09 09:34:30 +0300

    Time: 1984 ms (63.97%), Space: 51.4 MB (95.95%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -6,29 +6,24 @@ class Solution:
             i+=1
         heapify(tasks)
         
-        # print(tasks[0])
-        
         heap = []
         time = -1
         ans = []
+        
         while tasks or heap:
             if not heap and tasks[0][0] > time:
                 time = tasks[0][0]
-                # else:
-                #     time = tasks[0][0] + tasks[0][1]
-                    
+                
             while not heap or (tasks and tasks[0][0] <= time): 
                 temp = heappop(tasks)
                 temp[0],temp[1] = temp[1], temp[0]
                 temp[1],temp[2] = temp[2],temp[1]
                 heappush(heap,temp)
+                
             if heap:
                 temp = heappop(heap)
                 time+= temp[0]
                 ans.append(temp[1])
-#         print(heap)
-        
-#         print(ans)
         
         return ans
                 
