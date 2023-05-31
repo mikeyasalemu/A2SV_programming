ec94bb4748bf917acd619cc47d53ab68217502d9
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-05-31 22:35:01 +0300

    Time: 49 ms (26.05%), Space: 16.3 MB (21.22%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        state = [[0]* (n + 1) for _ in range(m+1)]
+        state[m-1][n-1] = 1
+        # print(state)
+        
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                state[i][j] += state[i+1][j] + state[i][j+1]
+        # print(state[0][0])
+        return state[0][0]
+        
