572aa8f97344530dacc61753b1ca5413769bf9bd
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-05-31 18:14:24 +0300

    Time: 46 ms (52.86%), Space: 16.3 MB (7.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
+        num1 = 0
+        num2 = 1
         if n == 1:
             return 1
+        elif n == 0:
+            return 0
         
-        return  self.fib(n-1) + self.fib(n-2)
+        for state in range(2,n+1):
+            temp = num2
+            num2 = temp + num1
+            num1 = temp
+        
+        return num2
