a5b242f9b48df48eac857ec1c022ec18084448c3
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-08-03 02:34:46 +0300

    added restore-ip-addresses.python

---
+++ restore-ip-addresses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        st = []
+        
+        def rec(ind, arr):
+            if ind < len(s) and len(arr) == 4:
+                    return
+            if ind > len(s) or len(arr) > 4:
+                return 
+            if ind == len(s):
+                if len(arr) == 4:
+                    print("yes",arr)
+                    st.append('.'.join(arr))
+                return
+           
+            # temp = deepcopy(arr)
+            if s[ind] == '0':
+                arr.append('0')
+                rec(ind + 1, arr)
+                arr.pop()
+            else:
+                for i in range(ind + 1, len(s) + 1):
+                    if 0 <= int(s[ind: i]) <= 255:
+                        arr.append(s[ind: i])
+                        rec(i, arr)
+                        arr.pop()
+                    else:
+                        break
+            
+
+        rec(0, [])
+        # print(st)
+        return st
