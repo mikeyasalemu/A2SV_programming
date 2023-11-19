1fd417921467429dcd21885fceada75e03b21b27
Mikeyas Alemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-11-19 22:59:37 +0300

    Added solution for Move Zeroes

---
+++ After-Camp-LeetCode/move-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        size = len(nums)
+        values = 0
+        index = values + 1
+        
+        while index < size:
+            
+            if nums[values] == 0 and nums[index] != 0:
+                nums[values], nums[index] = nums[index], nums[values]
+                values +=1
+                index +=1
+            elif nums[values] == 0 and nums[index] == 0:
+                index +=1
+            else:
+                values +=1
+                index +=1
+                
+        return nums
+            
+            
